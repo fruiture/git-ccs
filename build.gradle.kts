a6e06b7ae1bf675664ec4ae0bdeb00edee41ef37
Richard Wallintin <richard.wallintin@wps.de>
2024-02-08 21:38:29 +0100

    build: drop linux ARM due to missing clikt support

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,6 @@ kotlin {
     macosArm64 { binaries { executable() } }
     macosX64 { binaries { executable() } }
     linuxX64 { binaries { executable() } }
-    linuxArm64 { binaries { executable() } }
     mingwX64{ binaries { executable() } }
 
     sourceSets {
